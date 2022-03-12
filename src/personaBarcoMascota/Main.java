8f7c89469b6e5b8687d7c4c3fd8320d644b10bf3
ToniFeliudeC <afeliusalas@cifpfbmoll.eu>
2022-03-12 23:58:28 +0100

    Refactorizacion 4

--- src/personaBarcoMascota/Main.java
+++ src/personaBarcoMascota/Main.java
@@ -8,7 +8,7 @@ package personaBarcoMascota;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Refactorizacion 3");
+		System.out.println("Refactorizacion 4");
 
 
 	}
